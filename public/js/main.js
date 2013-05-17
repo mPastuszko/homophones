0532afd3ea3303e27fe54afb6273f248d4e9e39b
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-05-18 01:33:16 +0200

    Filter rendering added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
+Array.prototype.unique = function() {
+    var o = {}, i, l = this.length, r = [];
+    for(i=0; i<l;i+=1) o[this[i]] = this[i];
+    for(i in o) r.push(o[i]);
+    return r;
+};
+
 function loadTemplates() {
   Templates = {};
   $('script[type="text/jade-template"]').each(function(i) {
@@ -8,22 +15,40 @@ function loadTemplates() {
 
 function loadResults(word) {
   $.getJSON('/results.json', {word: Data.word}, resultsLoaded);
+  // resultsLoaded([]);
 }
 
 function resultsLoaded(data) {
   Data.results = data;
   var renderedResults = renderResults();
+  var renderedFilter = renderFilter();
   $('#results').html(renderedResults);
+  $('#filter').html(renderedFilter);
   $('#results-loader').addClass('hidden');
-  $('#results').removeClass('hidden');
+  $('#results, #filter').removeClass('hidden');
 }
 
-function renderResults(word) {
+function renderResults() {
   var data = {
     word: Data.word,
     homophones: Data.results
   };
   return Templates['results-template'](data);
+}
+
+function renderFilter() {
+  var data = {
+    styles: extractProperty(Data.results, 'style'),
+    syllables: extractProperty(Data.results, 'syllables')
+  };
+  return Templates['filter-template'](data);
+}
+
+function extractProperty(list, property) {
+  var prop_values = $.map(list, function(v, i) {
+    return v[property];
+  });
+  return prop_values.sort().unique();
 }
 
 $(function() {
