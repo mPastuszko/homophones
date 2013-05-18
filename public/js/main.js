8ec8626e900c2615daaf1b1cfc8abcafc2fe5787
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-05-18 17:01:56 +0200

    Filter engine implemented

--- public/js/main.js
+++ public/js/main.js
@@ -24,14 +24,31 @@ function resultsLoaded(data) {
   var renderedFilter = renderFilter();
   $('#results').html(renderedResults);
   $('#filter').html(renderedFilter);
+  setupFilterHandlers();
+
   $('#results-loader').addClass('hidden');
   $('#results, #filter').removeClass('hidden');
 }
 
-function renderResults() {
+function updateResults() {
+  var filterSettings = collectFilterSettings();
+  var results = filterResults(Data.results, filterSettings);
+  var renderedResults = renderResults(results);
+  $('#results').html(renderedResults);
+}
+
+function filterResults(results, filterSettings) {
+  return $.grep(results, function(e, i) {
+    return filterSettings['style'][e['style']] &&
+           filterSettings['syllables'][e['syllables']] &&
+           (e['frequency'] || !filterSettings['frequency']['on']);
+  });
+}
+
+function renderResults(results) {
   var data = {
     word: Data.word,
-    homophones: Data.results
+    homophones: results || Data.results
   };
   return Templates['results-template'](data);
 }
@@ -44,11 +61,30 @@ function renderFilter() {
   return Templates['filter-template'](data);
 }
 
+function setupFilterHandlers() {
+  $('#filter input').change(updateResults);
+}
+
 function extractProperty(list, property) {
   var prop_values = $.map(list, function(v, i) {
     return v[property];
   });
   return prop_values.sort().unique();
+}
+
+function collectFilterSettings() {
+  var filter_form_arr = $('#filter-form').serializeArray();
+  var settings = {
+    syllables: {},
+    style: {},
+    frequency: {}
+  };
+  $.each(filter_form_arr, function(i, e) {
+    var property = e['name'];
+    var value = e['value'];
+    settings[property][value] = true;
+  });
+  return settings;
 }
 
 $(function() {
